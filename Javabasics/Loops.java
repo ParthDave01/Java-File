1b9f7d8582d0ec93634ae7f2a0b5d8d0fa85d2e7
PathDave01 <dave.parthdave2004@gamil.com>
2024-07-03 23:06:41 +0530

    Loops

---
+++ Javabasics/Loops.java
@@ -0,0 +1,11 @@
+public class Loops {
+    public static void main(String args[]) {
+        int counter = 0;
+        while (counter < 50) {
+            System.out.println("Hello world");
+            counter++;
+        }
+
+        System.out.println("Printed Hw 50X");
+    }
+}
