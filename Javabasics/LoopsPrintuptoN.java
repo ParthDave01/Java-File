cdedda8928e7d0668eecccd2ebdb357bac6f2d41
PathDave01 <dave.parthdave2004@gamil.com>
2024-07-04 18:51:25 +0530

    LoopsUptoN

--- Javabasics/LoopsPrintuptoN.java
+++ Javabasics/LoopsPrintuptoN.java
@@ -3,11 +3,13 @@ import java.util.*;
 public class LoopsPrintuptoN {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
+
         System.out.println("enter the number :");
         int range = sc.nextInt();
         int counter = 1;
+        
         while (counter <= range) {
-            System.out.println(counter + " ");
+            System.out.println(counter);
             counter++;
         }
         System.out.println();
